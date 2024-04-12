36f423af8c0da37c5b198cdb45d838f327700714
hugodrw <hugoderohanwillner@gmail.com>
2024-04-12 16:26:12 +0200

    delete wandb

---
+++ main.py
@@ -0,0 +1,36 @@
+
+import os, sys
+import gymnasium as gym
+import time
+
+import text_flappy_bird_gym
+
+if __name__ == '__main__':
+
+    # initiate environment
+    env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 4)
+    obs = env.reset()
+
+    # iterate
+    while True:
+
+        # Select next action
+        action = env.action_space.sample()  # for an agent, action = agent.policy(observation)
+
+        # Appy action and return new observation of the environment
+        obs, reward, done, _, info = env.step(action)
+
+        # Render the game
+        os.system("clear")
+        sys.stdout.write(env.render())
+        print('Action: ', action)
+        print('Observation: ', obs)
+        print('Reward: ', reward)
+        time.sleep(1) # FPS
+        
+
+        # If player is dead break
+        if done:
+            break
+
+    env.close()
